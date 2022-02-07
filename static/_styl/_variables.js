11f2fffade77978e25c24511bd814e4cd7c25980
Terry Nguyen <terry@terrehbyte.com>
2022-02-07 01:52:44 -0800

    Remove styl files, use Inter font, bump SCSS files

--- static/_styl/_variables.js
+++
@@ -1,9 +0,0 @@
-// See https://github.com/stylus/stylus/issues/2014
-
-module.exports = function (vars) {
-  return function(stylus) {
-    for (var name in vars) {
-      stylus.define(name, vars[name]);
-    }
-  };
-};
